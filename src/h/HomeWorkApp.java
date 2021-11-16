71cb900ff730d2a755649917717879c00f17f4cb
KaterinichDanilo <danilokaterinich@gmail.com>
2021-11-16 22:43:12 +0200

    Hm1\1

--- src/h/HomeWorkApp.java
+++ src/h/HomeWorkApp.java
@@ -2,6 +2,10 @@ package h;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    public static void printThreeWords(){
+        System.out.println("Orange\nBanana\nApple");
     }
 }
