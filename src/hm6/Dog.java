739d0ef4b9ca43e63a3e09e86f226074b9e62b41
KaterinichDanilo <danilokaterinich@gmail.com>
2021-12-03 13:04:51 +0200

    Homework 6

---
+++ src/hm6/Dog.java
@@ -0,0 +1,8 @@
+package hm6;
+
+public class Dog extends Animals{
+
+    public Dog(String name, int possibleDistanceRun, int possibleDistanceSwim) {
+        super(name, possibleDistanceRun, possibleDistanceSwim);
+    }
+}
